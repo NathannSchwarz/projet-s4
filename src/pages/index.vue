4f719b3e0f116fc52931df02c4e0c843bedca4a6
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2025-03-14 10:37:19 +0100

    interaction video

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,11 +19,13 @@
       class="relative h-[60vh] overflow-hidden z-20 w-11/12 md:w-9/12 lg:w-5/12 md:-mt-6 xl:-mt-14 lg:h-[45vh] nonevideo"
     >
       <video
-        class="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full object-cover rounded-xl xl:rounded-3xl nonevideo"
+        class="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full object-cover rounded-xl xl:rounded-3xl nonevideo no-controls"
         src="/video/IUT.mp4"
         autoplay
         loop
         muted
+        playsinline
+
       ></video>
     </div>
 
