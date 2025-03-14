d627a644076751c4dac55c048fd27f6f3aa5a7f2
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2025-03-14 10:20:57 +0100

    accueil

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,47 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1>PPP</h1>
+  <section class="relative overflow-hidden py-30 xl:py-32 flex flex-col items-center mx-auto">
+    <!-- Image en arrière-plan -->
+    <img
+      class="absolute h-[75vh] md:h-[85vh] lg:h-[70vh] xl:h-[85vh] xl:w-1/3  object-cover z-0"
+      src="/img/Stars.webp"
+      alt=""
+    />
+
+    <!-- Titre principal -->
+    <h1 class="relative text-(--color-Blanc) text-outline z-10 text-center text-4xl xl:text-6xl">
+      EXPLOREZ
+    </h1>
+
+    <!-- Conteneur de la vidéo avec hauteur fixe -->
+    <div
+      class="relative h-[60vh] overflow-hidden z-20 w-11/12 md:w-9/12 lg:w-5/12 md:-mt-6 xl:-mt-14 lg:h-[45vh]"
+    >
+      <video
+        class="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full object-cover rounded-xl xl:rounded-3xl"
+        src="/video/IUT NFC Licence professionnelle Dosimétrie et radioprotection médicales (DORA) - IUT Nord Franche-Comté (1080p, h264).mp4"
+        autoplay
+        loop
+        muted
+      ></video>
+    </div>
+
+    <!-- Sous-titre légèrement sous la vidéo -->
+    <h2
+      class="relative text-(--color-Blanc) text-outline z-10 text-center text-3xl xl:text-5xl md:-mt-6 xl:-mt-10"
+    >
+      L'EUROPE
+    </h2>
+  </section>
 </template>
+
+<style>
+.text-outline {
+  text-shadow:
+    1px 1px 0 black,
+    -1px -1px 0 black,
+    1px -1px 0 black,
+    -1px 1px 0 black;
+}
+</style>
