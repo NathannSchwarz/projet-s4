8820afb74e85b7807d54206df83b177c92923a19
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2025-03-16 19:21:36 +0100

    ajout route link

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createWebHistory, createRouter } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createHead } from '@unhead/vue'
 
+const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
-
-const app = createApp(App)
 app.use(router)
 
+const head = createHead()
+app.use(head)
 app.mount('#app')
