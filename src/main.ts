7db3a10e3e1a949af1fa666efd6dd4e1362414da
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2025-03-16 19:24:32 +0100

    ancien main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createWebHistory, createRouter } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
-import { createHead } from '@unhead/vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-const app = createApp(App)
 const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+const app = createApp(App)
 app.use(router)
 
-const head = createHead()
-app.use(head)
 app.mount('#app')
